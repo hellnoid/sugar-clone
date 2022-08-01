821649332177049048ea6aabe9eb1beaf936d011
unknown <harshini.u18@iiits.in>
2022-08-01 15:43:16 +0530

    homepage

---
+++ modules/header.js
@@ -0,0 +1,97 @@
+let header = () => {
+    return `<div id="head1">
+            
+    <a href=""> <img src="https://media.sugarcosmetics.com/upload/Logo-static.jpg" id="logo"></a>
+        <div id="search">
+             <input id="search-bar" type="text" placeholder="Try Liquid Lipstick">
+       
+             <input id="search-icon" type="submit" value="Search">
+        </div>
+     
+        <img id="user" class="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png"> 
+       
+        <a id="login-signup" href="" >Login/Register</a>
+    
+  <!-----   icons --->
+    <a href=""><img id="heart" class="icon" src="https://cdn0.iconfinder.com/data/icons/glyphpack/51/heart-512.png">  </a>
+   <a href=""><img class="icon" src="https://cdn-icons-png.flaticon.com/512/7446/7446956.png">  </a>
+   <a href=""><img  class="icon" src="https://static.thenounproject.com/png/170070-200.png"></a>
+                    
+</div>
+
+
+<!-----------head2-->
+
+
+<div id="dropdown">
+    <ul >
+      <li >    <a  href="" class="main" >MAKEUP</a>
+          
+              <ul>
+                <li>
+                  <a href="" class="sub">LIPS</a>
+                </li>
+                <li>
+                  <a href="" class="sub">FACE</a>
+                </li>
+                <li>
+                  <a href="" class="sub">EYES</a>
+                </li>
+              </ul>
+           
+      </li>
+           
+      <li >  <a class="main" href="" >BRUSHES</a>
+          
+              <ul>
+                <li><a href="" class="sub">FACE BRUSHES</a></li>
+                <li><a href="" class="sub">EYE BRUSHES</a></li>
+              </ul>
+            
+      </li>
+      <li >  <a class="main" href="">SKINCARE</a>
+          
+              <ul>
+                <li><a href="" class="sub">MOISTURIZERS</a></li>
+                <li><a href="" class="sub">SUNSCREEN</a></li>
+                <li><a href="" class="sub">SETTING MISTS</a></li>
+                <li><a href="" class="sub">COFFE CULTURE RANGE</a></li>
+                <li><a href="" class="sub">SHEET MASK COMBO</a></li>
+                <li><a href="" class="sub">CITRUS GOT REAL RANGE</a></li>
+                <li><a href="" class="sub">AQUAHOLIC RANGE</a></li>
+                <li><a href="" class="sub">MASKS</a></li>
+              </ul>
+           
+      </li>
+      <li >    <a class="main" href="" >GIFTING</a>
+          
+              <ul>
+                <li><a href="" class="sub">SUGAR MERCH STATION</a></li>
+                <li><a href="" class="sub">MAKEUP KITS</a></li>
+                <li><a href="" class="sub">BEST SELLERS</a></li>
+                <li><a href="" class="sub">VALUE SETS</a></li>
+                <li><a href="" class="sub">SUGAR SETS</a></li>
+                <li><a href="" class="sub">E-GIFT CARDS</a></li>
+               
+              </ul>
+           
+      </li>
+      <li > <a href=""  class="main" >BLOG</a>
+          
+              <ul>
+                <li><a href="" class="sub">FEATURED</a></li>
+                <li><a href="" class="sub">MAKEUP</a></li>
+                <li><a href="" class="sub">SKINCARE</a></li>
+               
+              </ul>
+           
+      </li>
+      <li> <a class="main"
+          href="" >OFFERS</a></li>
+      <li> <a href="" class="main">STORES</a></li>
+      </ul>
+    </div>`
+
+
+}
+export { header }
