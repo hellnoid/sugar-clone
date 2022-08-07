d6116f33083c6749353eea8843c00a69e0e09f4e
sahid jamal <sahidjamal151@gmail.com>
2022-08-07 15:42:56 +0530

    fotter resolve

--- cart.js
+++ cart.js
@@ -85,7 +85,7 @@ var makeupData = [
     }];
 
 
-//localStorage.setItem("cartItems", JSON.stringify(makeupData));
+localStorage.setItem("cartItems", JSON.stringify(makeupData));
 
 
 
@@ -253,7 +253,7 @@ var makeupData = [
         var coupon = document.querySelector("#coupon").value;
         if(coupon=='sahid30'){
             document.querySelector(".invalid-coupon").textContent="";
-            document.querySelector(".valid-coupon").textContent="Coupon Applied Successfully";
+            document.querySelector(".valid-coupon").textContent="Coupon Applied";
             var discountPrice = total * 0.7;
 
             localStorage.setItem("discountPrice",discountPrice);
@@ -577,53 +577,4 @@ window.location.href = "cart.html"
 
 
 
-      //footer part
-
-
-      let footer=()=>{
-        return ` <div id="mainfooter">
-        <div id="footersugaricon">
-            <img src="https://in.sugarcosmetics.com/Footer_sugar_icon.png" alt="">
-        </div>
-        <div id="footersocialmedia">
-                <img src="https://in.sugarcosmetics.com/desc-images/facebook.svg" alt=""><img src="https://in.sugarcosmetics.com/desc-images/Instagram.svg" alt=""><img src="https://in.sugarcosmetics.com/desc-images/Pinterest.svg" alt=""><img src="https://in.sugarcosmetics.com/desc-images/Tumblr.svg" alt=""><img src="https://in.sugarcosmetics.com/desc-images/Youtube.svg" alt="">
-        </div>
-        <div id="twofooters">
-            <div id="tfleft">
-                <p style="color: white;font-weight:300;">SUBSCRIBE TO OUR NEWSLETTER</p>
-                <div id="footsignup"><input type="text" placeholder="Your e-mail address"><button>SIGN UP</button></div>
-                <p style="color:white;font-weight:300">INFORMATION</p>
-            </div>
-            <div id="thright">
-                <p style="color: white;font-weight:300;">GET THE SUGAR APP TODAY.</p>
-                <div id="thr1"><p>Tap into a better shopping experience.</p><div id="playstore"><img src="https://in.sugarcosmetics.com/playstore.png" alt=""><img src="https://in.sugarcosmetics.com/apple-store.png" alt=""></div></div>
-            </div>
-        </div>
-        <div id="write1">
-            <p>Stores</p>
-            <p>Terms & Conditions</p>
-            <p>Returns</p>
-            <p>FAQs</p>
-            <p>About Us</p>
-        </div>
-        <div id="git">GET IN TOUCH</div>
-        <div id="foot">
-            <div id="footseclast1"><div id="fut">
-                <img src="https://in.sugarcosmetics.com/desc-images/phone_android_white.svg" alt="">
-                <p >Call us at</p>
-            </div>
-            <p>1800-209-9933</p></div>
-            <div class="footseclast">
-                <p>Support</p>
-                <p id="sugarmail">hello@sugarcosmetics.com</p>
-            </div>
-            <div class="footseclast"><p>Careers</p><p>We're hiring!</p></div>
-            <div class="footseclast"><p>PRESS & MEDIA</p><p>pr@sugarcosmetics.com</p></div>
-            <div class="footseclast"><p>INFLUENCER COLLAB</p><P>JOIN US</P></div>
-        </div>
-        <hr>
-        <div id="footlast"><p>Copyright © 2022 SUGAR Cosmetics. All rights reserved.</p></div>
-    </div>`;
-    }
-    
-    export {footer};
+     
