74173df02b55eb4dc2cd4c5db8e323a7ef1462ce
Md Talib <95065465+talib789@users.noreply.github.com>
2022-08-09 09:11:26 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+let login = () => {
+  return `
+    <div id="signappend"></div>
+    <div id="navlogin">
+      <p>
+        <span><i class="fa-solid fa-user"></i></span>&nbsp;&nbsp;<div id="changename">Login/Register</div>
+      </p>
+    </div>`
+}
+export { login }
